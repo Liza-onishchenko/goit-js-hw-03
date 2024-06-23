0081c91c9315f7efa16403b1bc5e52e103bba94e
Liza <onishenkolizka99@gmail.com>
2024-06-23 19:55:01 +0300

    goit-js-hw-03

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+
+ const filterArray = function (numbers, value) {
+     let ressultArray = [];
+     for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            ressultArray.push(numbers[i]);
+        }
+     } return ressultArray;
+ } 
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
